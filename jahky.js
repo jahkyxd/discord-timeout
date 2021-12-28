82ef807a70f72aa0383a234fd7ddbcaf5c32dcf4
jahkyxd <82198115+jahkyxd@users.noreply.github.com>
2021-12-28 17:42:11 +0300

    Create jahky.js

---
+++ jahky.js
@@ -0,0 +1,9 @@
+member.timeout(time, reason);
+
+/** 
+* Şeklinde kullanabileceğiniz bir özelliktir.
+* member: kullanıcı tanımınız şeklinde değiştirebilirsiniz.
+* time: süre belirttiğiniz kısım.
+* reason: neden zaman aşımı uğrattığınız kısmıdır kısaca sebep.
+* basit bir şey alın kullanın.
+*/
